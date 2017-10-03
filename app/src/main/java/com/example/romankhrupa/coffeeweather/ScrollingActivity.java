4095935f371c604bc9052e849493d842cd814d6d
mikeborodin <streamofbee@gmail.com>
2017-10-03 13:17:47 +0300

    added location, permission check, notification functions and API client(but dont work for some reason)

--- app/src/main/java/com/example/romankhrupa/coffeeweather/ScrollingActivity.java
+++ app/src/main/java/com/example/romankhrupa/coffeeweather/ScrollingActivity.java
@@ -1,16 +1,42 @@
 package com.example.romankhrupa.coffeeweather;
 
+import android.Manifest;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.kwabenaberko.openweathermaplib.Units;
+import com.kwabenaberko.openweathermaplib.implementation.OpenWeatherMapHelper;
+import com.kwabenaberko.openweathermaplib.models.currentweather.CurrentWeather;
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import io.nlopez.smartlocation.OnLocationUpdatedListener;
+import io.nlopez.smartlocation.SmartLocation;
+import io.nlopez.smartlocation.location.config.LocationParams;
+import io.nlopez.smartlocation.location.providers.LocationGooglePlayServicesWithFallbackProvider;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
+
+import static java.security.AccessController.getContext;
 
 public class ScrollingActivity extends AppCompatActivity {
 
+    public final static String TAG = "ScrollingActivity";
+    OpenWeatherMapHelper helper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +50,59 @@ public class ScrollingActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                updateInfo();
+            }
+        });
+
+        helper = new OpenWeatherMapHelper();
+        helper.setApiKey(getString(R.string.OPEN_WEATHER_MAP_API_KEY));
+        helper.setUnits(Units.METRIC);
+
+/*
+* https://android-arsenal.com/details/1/6020
+* */
+        new RxPermissions(this).request(Manifest.permission.ACCESS_FINE_LOCATION).subscribe(new Consumer<Boolean>() {
+            @Override
+            public void accept(@NonNull Boolean ok) throws Exception {
+                if(ok){
+                    updateInfo();
+                }
+            }
+        });
+
+        SmartLocation.with(this)
+                .location( new LocationGooglePlayServicesWithFallbackProvider(getApplicationContext()))
+                .config(LocationParams.BEST_EFFORT)
+                .start(new OnLocationUpdatedListener() {
+            @Override
+            public void onLocationUpdated(Location location) {
+                Toast.makeText(ScrollingActivity.this,
+                        ""+location.getLatitude(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void updateInfo() {
+        Log.e(TAG, "updateInfo");
+        Location location = SmartLocation.with(this).location().getLastLocation();
+        //TODO: insert this location ->
+        helper.getCurrentWeatherByGeoCoordinates(29, 49, new OpenWeatherMapHelper.CurrentWeatherCallback() {
+            @Override
+            public void onSuccess(CurrentWeather currentWeather) {
+                Log.e(TAG,
+                        "Coordinates: " + currentWeather.getCoord().getLat() + ", "+currentWeather.getCoord().getLat() +"\n"
+                                +"Weather Description: " + currentWeather.getWeatherArray().get(0).getDescription() + "\n"
+                                +"Max Temperature: " + currentWeather.getMain().getTempMax()+"\n"
+                                +"Wind Speed: " + currentWeather.getWind().getSpeed() + "\n"
+                                +"City, Country: " + currentWeather.getName() + ", " + currentWeather.getSys().getCountry()
+                );
+                sendNotification();
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                Log.v(TAG, throwable.getMessage());
             }
         });
     }
@@ -47,5 +126,24 @@ public class ScrollingActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    int notificationId = 0;
+    void sendNotification(){
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!");
+        Intent resultIntent = new Intent(this, ScrollingActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(ScrollingActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        // mId allows you to update the notification later on.
+        mNotificationManager.notify(notificationId, mBuilder.build());
     }
 }
